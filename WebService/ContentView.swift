36f6fdff6cfadc4a0cd82c56ae6956aae3a901bd
Dayton Steffeny <daytonsteffeny@gmail.com>
2019-11-15 22:02:44 -0600

    Initial Commit

---
+++ WebService/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WebService
+//
+//  Created by Dayton Steffeny on 11/15/19.
+//  Copyright © 2019 Dayton Steffeny. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
